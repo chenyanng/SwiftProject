3e00adeff3ef6eeb0064340453c61ce261fa7de8
Chen Yang <1632455123@qq.com>
2021-07-05 01:04:59 +0800

    Initial Commit

---
+++ SwiftProject/SwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftProjectApp.swift
+//  SwiftProject
+//
+//  Created by 陈扬 on 2021/7/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
